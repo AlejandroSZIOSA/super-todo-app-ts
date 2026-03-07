613b27569021b37235a25dd2bf3e98a850469a94
Gabriel Sazo <ggato33@gmail.com>
2026-03-07 13:02:03 +0100

    added language flag

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,42 @@
 import { type FC, type ReactNode } from "react";
 import styles from "./Header.module.css";
 
+import { type RootState } from "../../store";
+import { useAppSelector } from "../../hooks/reduxHooks";
+
+import EN_FLAG from "../../assets/images/flags/en.png";
+import SV_FLAG from "../../assets/images/flags/sw.png";
+import ES_FLAG from "../../assets/images/flags/es.png";
+
 interface HeaderProps {
   children?: ReactNode;
 }
 
 //using constrains fix the problem with the children prop
 const Header: FC<HeaderProps> = ({ children }) => {
+  const { language } = useAppSelector((state: RootState) => state.settings);
+
+  let flagContent: string = ""; // Initialize flagContent with an empty string
+
+  switch (language) {
+    case "en":
+      flagContent = EN_FLAG;
+      break;
+    case "sv":
+      flagContent = SV_FLAG;
+      break;
+    case "es":
+      flagContent = ES_FLAG;
+      break;
+    default:
+      flagContent = EN_FLAG; // Default to English flag if language is not recognized
+  }
+
   return (
     <header>
       <div className={styles.appTitleContainer}>
-        <h1>Super Todo App</h1>
-        <button className={styles.animationBtn}>Ani Btn</button>
+        <h1>Task Organizer</h1>
+        <img src={flagContent} />
       </div>
       <div className={styles.childrenContainer}>{children}</div>
     </header>
---
+++ src/components/mobile-ui/SelectModal/SelectModalItem.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SelectModalItem = () => {
+  return <div></div>;
+};
+
+export default SelectModalItem;
